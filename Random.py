9705964e143116ec00d4b5242967211548bd6b94
TheValeHack <50583705+TheValeHack@users.noreply.github.com>
2019-06-28 23:02:08 +0700

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,330 @@
+# Coded by TheVale / Irfan VrnVrn
+# Gw Gans Tq.
+# Jangan Di Recode Bgst
+# Ini Tools Pertama Buatan Gw
+# hehehe
+print ""
+print "Coded by TheVale / Irfan Vrn"
+print "Email : thevale145@gmail.com"
+print "Instagram : @irfan_vrn"
+print "Ini Tools Python pertama bikinan Gw"
+print "Jadi maaf ya kalo Jelek :D"
+print "Enjoy My Tools :D"
+print ""
+print "-------------------------------"
+# Enjoy My Tools and don't Recode it >:(
+def menu():
+    print "Silahkan Dipilih Coeg !"
+    print ""
+    print "1.Pertambahan"
+    print "2.Pengurangan"
+    print "3.Perkalian"
+    print "4.Pembagian"
+    print "5.Quotes mantap"
+    print "6.Tutorial Nembak Cewek"
+    print "7.Cara menjadi Tampan"
+    print "8.Doa-Doa bermanfaat"
+    print "9.Coder's Profile"
+    print "10.Exit"
+    print ""
+    print "-------------------------------"
+# Pertambahan 
+def Pertambahan():
+    print "Tools Pertambahan untuk Anda yang mungkin Goblok"
+    print ""
+    a1=input("Angka 1 : ")
+    a2=input("Angka 2 : ")
+    h1=a1+a2
+    print "Hasil : ",h1
+    print ""
+    print "Mau ngitung lagi Apa kagak Boss? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+   
+# Pengurangan
+def Pengurangan():
+    print "Tools Pengurangan untuk Anda yang mungkin Goblok"
+    print ""
+    an1=input("Angka 1 : ")
+    an2=input("Angka 2 : ")
+    ha1=an1-an2
+    print "Hasil : ",ha1
+    print ""
+    print "Mau ngitung lagi apa kagak Boss? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+   
+ # Perkalian
+def Perkalian():
+     print "Tools Perkalian untuk Anda yang mungkin Goblok"
+     print ""
+     ang1=input("Angka 1 : ")
+     ang2=input("Angka 2 : ")
+     has1=ang1*ang2
+     print "Hasil : ",has1
+     print ""
+     print "Mau ngitung lagi apa kagak Boss? (Y/T)"
+     back=raw_input().upper()
+     if back=="Y":
+        menu()
+    
+ # Pembagian
+def Pembagian():
+     print "Tools Pembagian untuk Anda yang mungkin Goblok"
+     print ""
+     angk1=input("Angka 1 : ")
+     angk2=input("Angka 2 : ")
+     hasi1=angk1/angk2
+     print "Hasil : ",hasi1
+     print ""
+     print "Mau ngitung lagi apa kagak Boss? (Y/T)"
+     back=raw_input().upper()
+     if back=="Y":
+        menu()
+    
+# Quotes Mantap
+def Quotes():
+    print "Quotes Motivasi,Cinta,Persahabatan,dan Hidup"
+    print ""
+    print ""
+    print "Quotes Motivasi"
+    print "Gagal itu urusan nanti yang terpenting kita berani untuk mencoba dan mencoba~"
+    print ""
+    print "Tinggalkan pikiran yang membuatmu lemah, dan peganglah pikiran yang memberi kekuatan bagimu~"
+    print ""
+    print "Mereka bilang impian saya terlalu besar, saya bilang mereka berpikir terlalu kecil~"
+    print ""
+    print "Cara terbaik dan mudah untuk membuat orang mengingat anda adalah dengan keberhasilan mu~"
+    print ""
+    print "Bukanlah kesabaran jika masih mempunyai batas dan bukanlah keikhlasan jika masih merasakan sakit~"
+    print ""
+    print "~Semua masalah pasti akan berlalu dan berganti dengan kebahagiaan~"
+    print ""
+    print "~Prestasi tak dapat diraih tanpa semangat~"
+    print ""
+    print "~Keringat yang mengalir di medan latihan adalah penebus darah di medan pertempuran~"
+    print "Quotes Cinta"
+    print ""
+    print "~Jika kamu bertanya seperti apa kesempurnaan cinta. Jawabannya, cinta yang sempurna itu tanpa 'tapi'~"
+    print ""
+    print "~Tak ada yang salah dengan 'cinta.' Itu hanyalah sebuah kata dan kita sendirilah yang memaknainya~"
+    print ""
+    print "~Air mata adalah satu satunya cara untuk berbicara, di saat bibir kamu tidak mampu menjelaskan apa yang membuatmu terluka~"
+    print ""
+    print "~Cinta membawa kesenangan di dalam kebersamaan, kesedihan di dalam perpisahan, dan harapan di hari esok~"
+    print ""
+    print "~Cinta bukan tentang berapa lama kamu mengenalnya, melainkan tentang bagaimana kamu membuatnya tersenyum sejak mengenalnya~"
+    print ""
+    print "~Cinta tidaklah buta. Ia murni dan selalu diharapkan. Yang buta itu adalah saat cinta menguasai dirimu tanpa adanya sebuah pertimbangan~"
+    print ""
+    print "~Ketakutan terbesar dalam hidupku adalah ketika melihat kamu kehilangan rasa kebahagiaanmu~"
+    print ""
+    print "~Terkadang, air mata sebagai tanda kebahagiaan yang tak bisa diucapkan. Dan senyuman adalah tanda sakit yang mencoba untuk kuat~"
+    print "Quotes Persahabatan"
+    print ""
+    print "~Orang lain boleh datang dan pergi sesuka hati, tetapi sahabat sejati selalu tetap di hati.~"
+    print ""
+    print "~Tidak ada yang mustahil ketika kamu memiliki orang yang tepat untuk mendukung setiap langkahmu.~"
+    print ""
+    print "~Bagikan senyummu kepada dunia. Itu adalah lambang persahabatan dan perdamaian.~"
+    print ""
+    print "~Seorang sahabat adalah teman Anda yang tidak menghina pagarmu yang rusak, tetapi mengagumi bunga-bunga di kebun Anda.~"
+    print ""
+    print "~Jadilah pelangi dalam awan kelam seseorang.~"
+    print ""
+    print "~Berlian itu adalah bukanlah sahabat terbaik seorang wanita, tetapi sahabat itu adalah berlianmu.~"
+    print ""
+    print " ~Dalam perjalanan kehidupan, teman adalah kepingan yang indah.~"
+    print ""
+    print "~Banyak orang yang akan masuk dan keluar di hidup Anda, tetapi hanya seorang sahabat yang akan meninggalkan jejak di hati Anda~"
+    print ""
+    print "Quotes Hidup"
+    print ""
+    print "~Cinta tidak pernah menghambat seorang pria untuk mengejar takdirnya~"
+    print ""
+    print "~Ketakutan akan penderitaan lebih buruk daripada penderitaan itu sendiri,tidak ada hati yang menderita ketika berada dalam pencarian akan mimpi mimpinya~"
+    print ""
+    print "~Orang-orang memiliki kapabilitas setiap saat dalam kehidupannya untuk berjuang demi apa yang mereka impikan~"
+    print ""
+    print "~Ketika kau menginginkan sesuatu, Alam Semesta akan bersatu untuk membantumu menggapainya~"
+    print ""
+    print "~Rahasia hidup adalah jatuh tujuh kali dan bangun delapan kali~"
+    print ""
+    print "~Semua orang tampaknya memiliki gagasan yang jelas tentang bagaimana orang lain harus menjalani kehidupan mereka, tetapi tidak ada satupun gagasan yang jelas untuknya sendiri)~"
+    print ""
+    print "~Ingat bahwa di manapun hati Anda berada, di sanalah Anda akan menemukan harta Anda~"
+    print ""
+    print "~Orang-orang takut mengejar impian terbesar mereka karena mereka merasa bahwa impian-impian tersebut tidak pantas untuk mereka, atau mereka takut tidak akan pernah mencapai mimpi tersebut~"
+    print ""
+    print "Udah Baper/Termotivasi Belom Boss? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+      menu()
+   
+# Tutorial Nembak Cewek
+def Tutorial():
+    print "Anda Jomblo Ngenes dan Anda Ingin Sekali Punya Cewek untuk dihalalin?"
+    print "Tenang !!! Saya TheVale Adalah Pakar Cewek Handal ('Padahal Jomblo Akut :(..) dan Saya Siap Membantu Anda !!!"
+    print "Berikut adalah Langkah langkah Yang saya gunakan untuk menggait hati wanita (Padahal gk pernah nembak :(....)"
+    print ""
+    print "1.Tentukan dulu Siapa cewek yg sangat lu Cintai."
+    print "2.Buang Jauh Jauh Semua Nafsu kotorlu, dan Cintailah dia dengan tujuan Menghalalkannya."
+    print "3.Dekatilah dia dengan lembut tanpa paksaan dan sentuhan karena lu belum mahram."
+    print "4.Dekatilah dia dan pahami semua yang dia suka,Lalu Mulailah Mengobrol sampai Lu akrab."
+    print "5.Selalu Bersifat baiklah sama dia,dan selalu ingatkan dia untuk terus di jalan yang benar."
+    print "6.Apabila lu sudah melakukan semua langkah nya sekarang kita ke proses pentingnya."
+    print "7.Nyatakanlah perasaan lu ke dia dengan nada bicara yang sopan dan halus."
+    print "8.Ajaklah dia untuk Ta'aruf bersamalu agar bisa ke pelaminan."
+    print "9.Apabila Diterima Ya Alhamdulillah dan apabila ditolak Ya sabar dan cobalah untuk menjadi pribadi yg lebih baik."
+    print ""
+    print "Dah Mudah kan? Itulah cara cara yg harus kalian lakukan (Sebenernya gw ngasal sih :v)"
+    print ""
+    print "Udah Berhasil kan sekarang mau balik gak Boss? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+    
+# Cara Menjadi Tampan
+def Cara():
+    print "Muka anda Jelek? Akwkwokwok Kasian Amat lu tong."
+    print "Sabar Ya.. pasti karena mukalu jelek lu jadi milih menu ini."
+    print "Udah cuk sabar aja.. Muka Jelek itu bisa ditutupi dengan Sifat Lu yang bagus."
+    print "Walaupun Gw ganteng banget, gw tetep Menghargai Lu lok."
+    print "Mulai Sekarang Jadilah Pribadi yang lebih baik Dan dekatkanlah Diriku kepada Tuhan."
+    print "Sekali Lagi, Walaupun Gw sangat Tampan Gw tetep Respect dengan Kejelekan Lu kok."
+    print ""
+    print "Udah Berhasil Ganteng sekarang kan Boss :v? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+    
+# Doa-Doa Bermanfaat
+def Doa():
+    print "Bismillahirrahmanirrahim..."
+    print ""
+    print "Anda Ingin Menjadi pribadi yg lebih baik?"
+    print "Tenang saja saya disini juga mau Share Doa-Doa yang InsyaaAllah bisa bermanfaat :) "
+    print "Btw maaf ya , w gk bisa masukin Arabnya soalnya pas w masukin malah error"
+    print ""
+    print "1.Doa Mohon Kesholehan"
+    print ""
+    print "Robbanaa taqqobal minnaa innaka antas sami'ul 'aliim. Robbannaa waj'alna muslimaini "
+    print "laka wa min dzuriyyatinaa umatan muslimatan laka wa arinaa manasikanaa wa tub 'alainaa innaka antat tawwabur rohiim"
+    print ""
+    print "Artinya : Ya Allah, terimalah bakti kami. Sungguh Engkau Maha Mendengar lagi Maha Mengetahui."
+    print "Ya Allah, jadikan kami berdua orang yang taat pada Mu, dan jadikanlah keturunan kami umat yang patuh pada Mu."
+    print "Tunjukkan tata cara dan tempat ibadah haji kami, serta terimalah taubat kami. Sungguh Kau Maha Penerima taubat lagi Maha Penyayang"
+    print "(QS. Al Baqarah: 127 128)"
+    print "Penjelasan"
+    print "Doa ini dibaca Nabi Ibrahim ketika selesai membangun Kabah, yang waktu itu ditimpa banjir besar (zaman Nabi Nuh). Kemudian Nabi Ibrahim dibantu puteranya Ismail membangun kembali Ka'bah."
+    print ""
+    print "2.Doa Mohon Kebaikan Dunia Akhirat"
+    print "Robbanaa atinaa fid dunyaa hasanah wa fil aakhiroti hasanah waqinaa azabannaar"
+    print "Artinya : Ya Allah, berilah kami kebaikan di dunia dan kebaikan di akhirat, serta selamatkanlah kami dari siksa neraka."
+    print "(QS. Al Baqarah: 201)."
+    print "Penjelasan"
+    print "Dalam Al Quran dijelaskan bahwa doa ini dibaca oleh orang orang muslim yang ikhlas selesai melaksanakan haji. Ibnu Katsir menjelaskan bahwa doa ini mengandung seluruh kebaikan di dunia akhirat serta bisa menghindarkan dari segala kejelekan. Baik sekali doa ini dibaca pada setiap kesempatan"
+    print ""
+    print "3.Doa Tabah Menghadapi Lawan"
+    print "Robbanaa afrigh 'alainaa Sobron wa tsabit aqdaamanaa wansurnaa 'alal qoumil kaafiriin"
+    print "Artinya : Ya Allah, limpahkanlah kesabaran pada kami, kokohkan pendirian kami, serta tolonglah kami untuk mengalahkan orang orang kafir"
+    print "(QS. Al Baqarah: 250)"
+    print "Penjelasan"
+    print "Dijelaskan dalam Al Quran, bahwa doa ini dibaca oleh sekelompok mukmin yang bergabung dengan pasukan Thalut melawan jalut. Dengan doa ini dan atas izin Allah Swt. pasukan Thalut dapat mengalahkan Jalut, dan Nabi Daud membunuh Jalut."
+    print ""
+    print "4.Doa Keselamatan"
+    print "Robbanaa laa tuakzidnaa in nasiinaa wa akhto'naa robbanaa wa laa tahmil 'alainaa isron kama hamaltahu 'alal ladzIina min qoblinaa."
+    print "Robbanaa wa laa tuahamilnaa maa laa thooqoto lanaa bihi wa'fua 'anna waghfir lanaa warhamnaa anta maulaanaa fanshurnaa 'alal qoumil kaafiriin"
+    print "Artinya : Ya Allah, jangan siksa kami karena lupa atau bersalah. Ya Allah, jangan bebankan pada kami beban berat seperti Kau bebankan pada orang orang sebelum kami. Ya Allah, jangan Kau pikulkan pada kami apa yang tidak sanggup kami pikul. Ampunilah dan maafkan kami, serta rahmatilah kami. Kaulah Penolong kami, maka tolonglah kami untuk mengalahkan orang orang kafir."
+    print "(QS Al Baqarah 286)"
+    print "Penjelasan"
+    print "Ibnu Katsir merangkum sepuluh hadist tentang keutamaan membaca doa diatas. Diantaranya hadist: 'Barangsiapa yang membaca dua ayat akhir surah Al Baqarah (ayat 285 286) setiap malam, maka dia akan mendapatkan keselamatan.'"
+    print "5.Doa Menghindari kesesatan"
+    print "Robbanaa laa tuzigh quluubanaa ba'da idz hadaitanaa wa hab lanaa min ladunka rohmatan innaka antal wahh"
+    print "Artinya : Ya Allah, jangan Kau jadikan hati kami condong pada kesesatan sesudah Kau beri petunjuk pada kami, dan rahmatilah kami. Sungguh Kau Maha Pemberi karunia"
+    print "Penjelasan"
+    print "Doa ini dibaca oleh ahli ilmu yang beriman kepada keagungan Al Qur'an. Dan mereka berdoa kepada Allah Swt. agar tetap dalam jalan kebenaran, tidak condong kepada kesesatan setelah mendapatkan petunjuk, serta memohon curahan Rahmat Nya."
+    print ""
+    print "6.Doa Husnul Khatimah"
+    print "Robbanaa innanna sami'naa munadiyan yunaadi lil iiman an aamanu birobikun fa amannaa. Robbanaa faghfir lanaa dzunubaana wa kafir sayyiatinaa wa taffannaa ma'al abror. Robbanaa wa aatinaa ma wa'attanaa 'ala rusulika wa laa tukhzinaa yaumal qiyamah innaka laa tukhliful mi'aad"
+    print "Artinya : Ya Allah sungguh kami telah mendengar seruan yang menyeru pada iman: 'Berimanlah kamu kepada Allah, maka kami pun beriman. Ya Allah, ampunilah dosa dan hapuskan kesalahan kesalahan kami, serta matikan kami beserta orang orang yang banyak berbuat kebajikan. Ya Allah, berilah apa yang telah Engkau janjikan kepada kami dengan perantaraan Rasul Rasul Mu, dan janganlah Engkau hinakan kami pada hari kiamat nanti. Sungguh Engkau sama sekali tidak akan pernah menyalahi janji '"
+    print "(QS Ali Imron 193 294)"
+    print "Penjelasan"
+    print "Baik sekali doa di atas dibaca pada setiap kesempatan, tapi lebih utama pada waktu tengah malam (sepertiga malam) sampai menjelang shubuh. Karena ayat ini pula yang dibaca Nabi Saw. ketika bangun dari tidurnya sambil memandang langit. (HR. Bukhari)"
+    print ""
+    print "7.Doa Taubat"
+    print "Robbanaa dzolamnaa anfusanaa wa inlam taghfir lanaa wa tarhamnaa lanakunnaa minal khoosiriin"
+    print "(QS Al A'rof 23)"
+    print "Artinya : Ya Allah, kami telah menganiaya diri kami sendiri, dan jika Kau tidak mengampuni kami serta memberi rahmat pada kami, niscaya kami termasuk orang orang yang rugi."
+    print "Penjelasan"
+    print "Doa ini doa Nabi Adam dan isterinya Hawa, ketika keduanya dikeluarkan dari surga."
+    print ""
+    print "8.Doa Tolak Ke Zoliman"
+    print "Robbanaa laa taj'alnaa ma'al qoumil dzoolimin"
+    print "(Al A'rof 47)"
+    print "Artinya : Ya Allah, jangan Kau tempatkan kami bersama Doa dia atas dibaca oleh Ahlul A'rof, yakni orang orang yang amal kebajikan dan kejahatannya seimbang. Ketika mereka melihat ahli surga dengan berbagai kenikmatanya, mereka berkata: 'Kesejahteraan semoga tercurah bagi kalian, wahai ahli surga.' Dan ketika mereka melihat ahli neraka dengan berbagai siksanya, mereka berkata: 'Kami berlindung kepada Allah dari apa yang sedang kalian alami, Wahai ahli neraka.' Lalu mereka berdoa dengan doa diatas, yaitu memohon agar tidak disatukan dengan orang orang zholim.sama dengan orang orang zhalim"
+    print "Penjelasan"
+    print "Doa dia atas dibaca oleh Ahlul A'rof, yakni orang orang yang amal kebajikan dan kejahatannya seimbang. Ketika mereka melihat ahli surga dengan berbagai kenikmatanya, mereka berkata: 'Kesejahteraan semoga tercurah bagi kalian, wahai ahli surga.' Dan ketika mereka melihat ahli neraka dengan berbagai siksanya, mereka berkata: 'Kami berlindung kepada Allah dari apa yang sedang kalian alami, Wahai ahli neraka.' Lalu mereka berdoa dengan doa diatas, yaitu memohon agar tidak disatukan dengan orang orang zholim."
+    print ""
+    print "Udah Lebih Baik belom Boss Sekarang :) ? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+    
+# My Profile
+
+def Coder():
+    print "Hmmm... Lu mau Kepoin gw ya ?!?!"
+    print "Oke Seloww Gapapa."
+    print ""
+    print "Yoo Hallo Minna Gimana? Lu Enjoy ama Tools Gw Gak hehe :)"
+    print "Tools ini murni gw bikin sendiri,tanpa Recode,dan tanpa bantuan siapa-siapa."
+    print "Jadi Please Jangan di Recode bgst , gw udh dapet cape bikin malah di Recode -_-."
+    print ""
+    print "Coder's profile"
+    print "Nick : TheVale"
+    print "Gender : Laki-Laki"
+    print "Real Name : Irfan (Nama panjangnya lu gk perlu tau bgst)"
+    print "Age : 14 Taon hehe masih muda kan? :v"
+    print "Instagram : @irfan_vrn"
+    print "Contacts me : thevale145@gmail.com"
+    print "Greetz For : Corazon,NHCAT,PhobiaXploit,Friends Cyber Muslim, E.S.A ,Forum Indores"
+    print "Phoenix Cyber Army , Anonymous Cyber Muslim,Clay Attacker Team,dan Semua Grup yang gw Join"
+    print "dan juga semua Temen Gw tentunya :) "
+    print ""
+    print "Udah Puas Keponya? :) ? (Y/T)"
+    back=raw_input().upper()
+    if back=="Y":
+       menu()
+
+menu()
+while 1:
+   pilih=input('Pilih yang mana lu coeg? :')
+   if pilih==1:
+      Pertambahan()
+   elif pilih==2:
+      Pengurangan()
+   elif pilih==3:
+      Perkalian()
+   elif pilih==4:
+      Pembagian()
+   elif pilih==5:
+        Quotes()
+   elif pilih==6:
+      Tutorial()
+   elif pilih==7:
+      Cara()
+   elif pilih==8:
+      Doa()
+   elif pilih==9:
+      Coder()
+   elif pilih==10:
+      exit()
+ # Dah Segitu aja Coeg
+ # Gw bikin ginian Sendiri Tanpa bantuan / Recode
+ # So maaf ya kalau jelek
+ # Gw pertama kali main python soalnya hehe
+   
+    
